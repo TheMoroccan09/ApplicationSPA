640a2eccec0189ff1836cb02b8e6675580bebd55
themoroccan09 <themoroccan09@github.com>
2019-05-15 13:25:16 +0000

    Update title dynamically

--- ApplicationSPA/Scripts/App/App.js
+++ ApplicationSPA/Scripts/App/App.js
@@ -1,26 +1,47 @@
 ﻿var app = angular.module('App', ['ui.router']);
 
-
+// Configurations ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 }]);
 
 // Routing 
-app.config(function ($stateProvider, $locationProvider) {
-    $stateProvider
-        .state('home', {
-            url: '/',
-            template: '<h1> Template page home</h1>'
-        })
-        .state('about', {
-            url: '/Home/About',
-            templateUrl: '/Home/About',
-        })
-        .state('contact', {
-            url: '/Home/Contact',
-            templateUrl: '/Home/Contact'
-        })
+app.config(['$stateProvider', '$locationProvider',
+    function ($stateProvider, $locationProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<h1> Template page home</h1>', // TODO use templateUrl instead
+                data: { title: 'Home'}
+            })
+            .state('about', {
+                url: '/Home/About',
+                templateUrl: '/Home/About',
+                data: {title : 'About'}
+            })
+            .state('contact', {
+                url: '/Home/Contact',
+                templateUrl: '/Home/Contact',
+                data: {title: 'Contact'}
+            })
 
-    $locationProvider.html5Mode(true);
-});
+        $locationProvider.html5Mode(true);
+    }]);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// update title dynamically
+app.run(['$rootScope', '$transitions','$state',
+  function ($rootScope, $transitions, $state) {
+    $rootScope.title = 'My Super app'
+
+    $transitions.onSuccess({}, function () {
+        $rootScope.title = $state.$current.data.title
+    })
+  }
+
+])
+
+
 
